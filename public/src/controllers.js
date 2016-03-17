f7cfc0c26d5e7a840a5404bc86d2851dd3bfd2fa
Mindaugas Sasnauskas <mindaugas.s@wilibox.com>
2016-03-17 16:58:15 +0200

    working service, cant add playslists

--- public/src/controllers.js
+++ public/src/controllers.js
@@ -19,44 +19,6 @@ function searchController($rootScope, $scope, googleService) {
     }, function (error) {
       console.log('Failed: ' + error)
     });
-    console.log("$scope.searchResults " + JSON.stringify($scope.searchResults));
-
-
-
-
-    // console.log($scope.searchModel);
-    // var q = $scope.searchModel;
-    // SearchFactory(q, 'snippet');
-    // $scope.searchResults = $rootScope.searchResults
-    // console.log($scope.searchResults);
-
-
-
-
-
-    // SearchFactory.searchResult().then(function (data) {
-    //   console.log(data + " data");
-    //   //   $scope.Temp1 = data.Temp1;
-    // });
-    // console.log($search.value);
-
-    // var request = gapi.client.youtube.search.list({
-    //   q: $scope.search.value,
-    //   part: 'snippet',
-    //   maxResults: 10
-    //     // type:'channel'
-    // });
-    // console.log($scope.search);
-    // request.execute(function(response) {
-    //
-    //   var str = JSON.stringify(response.result);
-    //   console.log(response.result);
-    //   var channelTitle = JSON.stringify(response.result.items[0].snippet.title);
-    //   // $scope.searchResults(str);
-    //   $scope.searchResults = response.result;
-    //   console.log($scope.searchResults.items + " $scope.searchResults");
-    // });
-    // list.search();
   }
 
 }
@@ -69,12 +31,30 @@ function subscriptionsController($window, $rootScope, $scope, $http, $sce, googl
   };
 
   $scope.showSubscriptions = function () {
-    googleService.googleApiClientReady().then(function (data) {
-      $scope.channel = data;
-      console.log($scope.channel);
+    googleService.googleApiClientReady("Subscriptions").then(function (data) {
+      $scope.subscriptionsResult = data;
+      console.log($scope.subscriptionsResult);
+      for (var i = 0; i < $scope.subscriptionsResult.items.length; i++) {
+        googleService.googleApiClientReady(
+          "Paylists",
+          $scope.subscriptionsResult.items[i].snippet.resourceId.channelId
+        ).then(function (data) {
+
+          var playlists = data;
+
+          $scope.subscriptionsResult.items[i][playlists] = {};
+          $scope.subscriptionsResult.items[i][playlists] = playlists
+          console.log($scope.subscriptionsResult.items[i].playlists);
+
+        }, function (error) {
+          console.log('Failed: ' + error)
+        });
+      }
+
     }, function (error) {
       console.log('Failed: ' + error)
     });
+
   };
 
 
@@ -109,20 +89,6 @@ function subscriptionsController($window, $rootScope, $scope, $http, $sce, googl
     //       part: 'snippet,contentDetails',
     //       maxResults: 5
     //     }
-    //
-    //
-    //
-    //     $http({
-    //         method: 'GET',
-    //         url: 'https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=UC5d4piMBQlBQRFpS9m_8UZQ'}).then(function successCallback(response) {
-    //         console.log(response.result);
-    //         console.log($scope.subscriptionsResult);
-    //       }, function errorCallback(response) {
-    //         console.log(response);
-    //       });
-    //
-    //
-    //
     //       var request = gapi.client.youtube.channels.list({
     //         id: $scope.subscriptionsResult.items[i].snippet.resourceId.channelId,
     //         part: 'snippet,contentDetails',
--- public/src/factories.js
+++ public/src/factories.js
@@ -1,40 +1,41 @@
 angular.module('youtApp')
-  // .factory('SearchFactory', ['$rootScope', function ($rootScope) {
-  //   var searchResult = function (q, part) {
-  //     var request = gapi.client.youtube.search.list({
-  //       q: q,
-  //       part: part // 'snippet'
-  //     });
-  //     request.execute(function (response) {
-  //       $rootScope.searchResults = response.result;
-  //     });
-  //   }
-  //   return searchResult;
-  // }])
   .service('googleService', ['$http', '$q', function ($http, $q) {
 
-    
+
     this.googleApiClientReady = function (type, q) {
       var deferred = $q.defer();
-       gapi.client.setApiKey('AIzaSyDz-n5ZL3bbWXP0eHdUWOoPFCrRlbKxluk');
+      maxResults = 3;
+      gapi.client.setApiKey('AIzaSyDz-n5ZL3bbWXP0eHdUWOoPFCrRlbKxluk');
       gapi.client.load('youtube', 'v3', function () {
         if (type == "search") {
           console.log(q);
           var request = gapi.client.youtube.search.list({
             q: q,
-            maxResults: 3,
+            maxResults: maxResults,
             part: 'snippet'
           });
-        }
-        // else if(type == "PaylistItems") {
-        //   var request = gapi.client.youtube.playlistItems.list({
-        //     q: q,
-        //     part: 'snippet',
-        //      playlistId: 'PLila01eYiSBjOtR8oqXkY0i5c1QS6k2Mu',
-        //     maxResults: 8
-        //   });
-        // }
-        else {
+        } else if (type == "Subscriptions") {
+          var request = gapi.client.youtube.subscriptions.list({
+            part: 'snippet,contentDetails',
+            type: 'channel',
+            mine: true,
+            maxResults: maxResults
+          });
+        }else if (type == "Paylists") {
+          console.log(q);
+          var request = gapi.client.youtube.playlists.list({
+            part: 'snippet,contentDetails',
+            channelId: q,
+            maxResults: 3
+          });
+        } else if (type == "PaylistItems") {
+          var request = gapi.client.youtube.playlistItems.list({
+            q: q,
+            part: 'snippet',
+            playlistId: 'PLila01eYiSBjOtR8oqXkY0i5c1QS6k2Mu',
+            maxResults: maxResults
+          });
+        } else {
           console.log("unknown type");
           return;
         }
